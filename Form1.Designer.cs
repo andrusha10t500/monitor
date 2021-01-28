a3fcbc05a012de536cf53de07f8be16f68a08eee
andrusha10t500 <andrusha10t500@gmail.com>
2021-01-28 18:03:01 +1000

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace CheckOnPC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "период обновления";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(147, 183);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(144, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Добавить ПК";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(147, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(147, 239);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "сохранить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 210);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Включить таймер";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(245, 149);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "1";
+            this.Column1.HeaderText = "компьютер";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "состояние";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Column2.ToolTipText = "Ежели галочка стоит - то компьютер включён";
+            this.Column2.TrueValue = "";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 183);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(100, 20);
+            this.numericUpDown1.TabIndex = 14;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged_1);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.ClientSize = new System.Drawing.Size(550, 447);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows;
+using System.Windows.Input;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.IO;
+
+
+
+namespace CheckOnPC
+{
+    public partial class Form1 : Form
+    {
+        private static String pc_name;
+        private static bool check; //вкл/выкл
+        public Form1()
+        {
+            const string Path = @"D:Proga.txt";
+            InitializeComponent();
+            timer1.Interval=Int32.Parse(this.numericUpDown1.Value.ToString())*1000;
+            if (File.Exists(Path))
+            {
+                var result = MessageBox.Show("Найдены сохранения, восстановить?", "Recovery", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    recovery();
+                }
+            }
+        }
+        public bool Thread_StateCheck(string pc_name1)
+        {            
+            pc_name = pc_name1;             
+            threadStart();
+            return check;            
+        }
+        public void threadStart()
+        {
+        //пинг компов  
+            try
+            {
+                PingReply pir;
+                Ping pi = new Ping();
+                pir = pi.Send(pc_name);
+                if (pir.Status != IPStatus.Success)
+                {
+                    check = false;
+                }
+                else
+                {
+                    check = true;
+                }
+            }
+            catch (Exception)
+            {
+                check = false;
+            }
+        }
+        //включение таймера
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {                
+                this.button3.Text = "Включить таймер";
+                this.timer1.Enabled = false;                
+            }
+            else
+            {
+                this.button3.Text = "Выключить таймер";
+                this.timer1.Enabled = true;
+                this.timer1.Start();                
+            } 
+        }
+        //Таймер событие 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Thread tr = new Thread(new ThreadStart(timer_custom));
+            tr.SetApartmentState(ApartmentState.MTA);
+            tr.Start();
+        }
+        //Timer в поток
+        public void timer_custom()
+        {
+            bool off = false;
+            string CompOff = "";
+            for (int i = 0; i <= this.dataGridView1.Rows.Count - 2; i++)
+            {
+                //if (StateCheck(dataGridView1.Rows[i].Cells[0].Value.ToString())) //--распоточил (см check)
+                if (Thread_StateCheck(dataGridView1.Rows[i].Cells[0].Value.ToString()))
+                {
+                    dataGridView1.Rows[i].Cells[1].Value = true;
+                }
+                else
+                {
+                    dataGridView1.Rows[i].Cells[1].Value = false;
+                }
+
+                if (dataGridView1.Rows[i].Cells[1].Value.Equals(false) && this.WindowState == FormWindowState.Minimized) //если выключен
+                {
+                    off = true;
+
+                    if (CompOff == "")
+                    {
+                        CompOff = this.dataGridView1.Rows[i].Cells[0].Value.ToString() + " Выключен";
+                    }
+                    else
+                    {
+                        CompOff = CompOff + "\n" + this.dataGridView1.Rows[i].Cells[0].Value.ToString() + " Выключен";
+                    }
+
+                    switch (i)
+                    {
+                        case 0:
+                            notifyIcon1.Icon = Properties.Resources.FirstOff;
+                            break;
+                        case 1:
+                            notifyIcon1.Icon = Properties.Resources.SecondOff;
+                            break;
+                        case 2:
+                            notifyIcon1.Icon = Properties.Resources.ThirdOff;
+                            break;
+                        case 3:
+                            notifyIcon1.Icon = Properties.Resources.FourthOff;
+                            break;
+                    }
+                }
+                if (off == false)
+                {
+                    if (i == this.dataGridView1.Rows.Count - 2)
+                    {
+                        notifyIcon1.Icon = Properties.Resources.AllOn;
+                        CompOff = "Все компьютеры включены";
+                    }
+                }
+            }
+            if (CompOff != "Все компьютеры включены")
+            {
+                notifyIcon1.Text = CompOff;
+                notifyIcon1.ShowBalloonTip(timer1.Interval, "Информация",
+                    CompOff, ToolTipIcon.Info);
+            }
+            else
+            {
+                notifyIcon1.Text = CompOff;
+            }
+        }
+        //добавление в грид
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.dataGridView1.Rows.Add(this.textBox2.Text,
+                           Thread_StateCheck(this.textBox2.Text));
+        }        
+        //изменение интервала таймера     
+        private void numericUpDown1_ValueChanged_1(object sender, EventArgs e)
+        {
+            timer1.Interval = Int32.Parse(numericUpDown1.Value.ToString())*1000;
+        }
+        //передача в файл
+        private void button2_Click(object sender, EventArgs e)
+        {
+            const string Path = @"D:Proga.txt";
+            using (StreamWriter sw = File.CreateText(Path))
+            {
+                try
+                {
+                    for (int i = 0; i <= this.dataGridView1.Rows.Count-2; i++)
+                    {
+                        sw.WriteLine(this.dataGridView1.Rows[i].Cells[0].Value.ToString());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                sw.Close();
+            }     
+        } 
+        //восстановление
+        public void recovery()
+        {
+            {
+                const string Path = @"D:Proga.txt";
+                string[] col_str = new string[InFile(Path)];
+                using (StreamReader file = File.OpenText(Path))
+                {
+                    int i = 0;
+                    while (!file.EndOfStream)
+                    {
+                        col_str.SetValue(file.ReadLine(), i);
+                        i++; //количество записей  
+                    }
+
+                    for (int j = 0; j <= i - 1; j++)
+                    {
+                        try
+                        {
+                            this.dataGridView1.Rows.Add(col_str[j].ToString(),
+                                Thread_StateCheck(col_str[j].ToString()));
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+                    file.Close();
+                }
+            }        
+        }                
+        //Количество строк в файле
+        public int InFile(string FilePath)
+        {
+            int number = 0;
+            using (StreamReader file1 = File.OpenText(FilePath))
+            {                 
+                while (!file1.EndOfStream)
+                {
+                    number++;
+                    file1.ReadLine();
+                }
+                file1.Close();                
+            }
+            return number;                        
+        }      
+        //В статус бар
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.ShowInTaskbar = false;
+                notifyIcon1.Visible = true;                
+            }
+        }       
+        //двойное нажатие на иконку в трее
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = false;
+            this.ShowInTaskbar = true;
+            this.WindowState = FormWindowState.Normal;
+        }              
+    }
+}
